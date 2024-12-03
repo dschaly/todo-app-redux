501b62694a3b8c067a8524cf300751901f19bee1
Davidson Schaly <davidson.schaly@gmail.com>
2024-12-02 21:41:17 -0300

    feat - redux configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Provider } from "react-redux"
+import store from "./redux/store"
+
+
 function App() {
 
   return (
-    <>
-     
-    </>
+    <Provider store={store}>
+      <h1 className="text-5xl">aaa</h1>
+    </Provider>
   )
 }
 
-export default App
+export default App;
